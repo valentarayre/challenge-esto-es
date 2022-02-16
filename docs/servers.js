579d610750e5c1eb2ffba2cb69db1fb901517323
valentarayre <valentintarayre@gmail.com>
2022-02-16 17:12:25 -0300

    fix swagger heroku server

--- docs/servers.js
+++ docs/servers.js
@@ -1,7 +1,13 @@
+const config = require('../config/config')
+
 module.exports = {
   servers: [
     {
-      url: 'http://localhost:3000/',
+      url: `http://localhost:${config.PORT}/`,
+      description: 'Development server'
+    },
+    {
+      url: 'https://agenciadigital123.herokuapp.com/',
       description: 'Development server'
     }
   ]
