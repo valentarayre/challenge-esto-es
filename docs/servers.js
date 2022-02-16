2b09424a8717ba756b2fda3a90dc7dd4417f0182
valentarayre <valentintarayre@gmail.com>
2022-02-16 19:39:07 -0300

    change priority

--- docs/servers.js
+++ docs/servers.js
@@ -3,11 +3,11 @@ const config = require('../config/config')
 module.exports = {
   servers: [
     {
-      url: `http://localhost:${config.PORT}/`,
-      description: 'Development server'
+      url: 'https://agenciadigital123.herokuapp.com/',
+      description: 'Heroku server'
     },
     {
-      url: 'https://agenciadigital123.herokuapp.com/',
+      url: `http://localhost:${config.PORT}/`,
       description: 'Development server'
     }
   ]
